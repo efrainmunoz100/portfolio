9242e735faadcf40ef75588bb38179c6428ec962
Efrain Munoz <63368883+efrainmunoz100@users.noreply.github.com>
2023-05-04 12:30:30 -0400

    added modals on click to cards

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$("#sps-card").on("click", () => {$("#sps-modal").fadeIn(75)})
+$(".close-sps").on("click", () => {$("#sps-modal").fadeOut(75)})
+
+$("#bitcoin-card").on("click", () => {$("#bitcoin-modal").fadeIn(75)})
+$(".close-bitcoin").on("click", () => {$("#bitcoin-modal").fadeOut(75)})
+
+$("#planetary-simulator-card").on("click", () => {$("#planetary-simulator-modal").fadeIn(75)})
+$(".close-planetary-simulator").on("click", () => {$("#planetary-simulator-modal").fadeOut(75)})
+
+$("#app-challenge-card").on("click", () => {$("#app-challenge-modal").fadeIn(75)})
+$(".close-app-challenge").on("click", () => {$("#app-challenge-modal").fadeOut(75)})

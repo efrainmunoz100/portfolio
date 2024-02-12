7cd24a57ac1cc95621dbeb265ddb7f4f94eb8dba
Efrain Munoz <63368883+efrainmunoz100@users.noreply.github.com>
2024-02-11 22:17:16 -0800

    resizing fixes

--- js/custom.js
+++ js/custom.js
@@ -14,10 +14,38 @@ $(".nav-link, .navbar-brand").on("click", () => {
   $(window).width() < 992 ? closeNav() : null;
 })
 
+$(window).on('resize', () => {
+  $(window).width() >= 992 ? openNav() : closeNav();
+})
+
+$(window).on('scroll', handleScroll);
+
 function openNav() {
   document.documentElement.style.setProperty('--navbar-offset', '250px');
 }
 
 function closeNav() {
   document.documentElement.style.setProperty('--navbar-offset', '0px');
+}
+
+// Function to check if an element is in the viewport
+function isInViewport(element) {
+  console.log(element);
+  const rect = element[0].getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function handleScroll() {
+  logos = $('.logo');
+  console.log(logos);
+  logos.each((logo) => {
+    if (isInViewport(logo)) {
+      icon.css("margin-bottom", "0"); // Move the icon to its final position
+      // $(window).off("scroll", handleScroll); // Remove the scroll event listener
+  }});
 }
